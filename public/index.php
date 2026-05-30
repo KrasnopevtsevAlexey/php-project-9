82c1eec24d837caadf576ce1de01e3fc53d6adbe
AlexeyKrasnopevtsev <fktrctq321@gmail.com>
2026-05-30 11:42:57 +0700

    second commit

--- public/index.php
+++ public/index.php
@@ -1,1 +1,19 @@
 <?php
+
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$app = AppFactory::create();
+
+// Добавляем обработчик ошибок для отладки
+$errorMiddleware = $app->addErrorMiddleware(true, true, true);
+
+$app->get('/', function (Request $request, Response $response) {
+    $response->getBody()->write('<h1>Page Analyzer</h1><p>Welcome to the Page Analyzer!</p>');
+    return $response;
+});
+
+$app->run();
